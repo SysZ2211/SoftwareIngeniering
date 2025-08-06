5dd7063b6b96b140ba28fa2122575434c1d3522c
SysZ2211 <anothersamwithz@gmail.com>
2025-08-06 09:11:57 -0500

    Creacion del proyecto

---
+++ Actividad1/src/main/java/co/edu/poli/Actividad1/vista/SecondaryController.java
@@ -0,0 +1,12 @@
+package co.edu.poli.Actividad1.vista;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
